7ed8ad2b118224627756310e19861af68c7d2aea
Saksham Mathur <saksham.mathur25@gmail.com>
2022-09-27 08:48:17 +0530

    some changes

--- components/SignUpScreen.js
+++ components/SignUpScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-paper';
 import AppView from './AppView';
 import Header from './Header';
@@ -7,6 +6,7 @@ export const SignUpScreen = () => {
   return (
     <AppView>
       <Header />
+      <Text>Form</Text>
     </AppView>
   );
 };
